760325f9d3b3080450a6046f0c527844ac7cf25c
Michael Ambrus <ambrmi09@gmail.com>
2014-11-05 01:19:45 +0100

    init_fini.c added
    A generic mechanism for initializing libraries without having to use any
    specific pre-known function.

--- host/libraries/libbladeRF/src/bladerf.c
+++ host/libraries/libbladeRF/src/bladerf.c
@@ -22,9 +22,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
-#if defined(__linux__)
-#include <syslog.h>
-#endif
 #include "rel_assert.h"
 
 #include "libbladeRF.h"     /* Public API */
@@ -1093,10 +1090,6 @@ const char * bladerf_strerror(int error)
 
 void bladerf_version(struct bladerf_version *version)
 {
-#if !defined(WIN32) && !defined(__CYGWIN__)
-    openlog("bladeRF",
-        LOG_CONS | LOG_NDELAY  | LOG_NOWAIT | LOG_PERROR | LOG_PID, LOG_USER);
-#endif
     version->major = LIBBLADERF_VERSION_MAJOR;
     version->minor = LIBBLADERF_VERSION_MINOR;
     version->patch = LIBBLADERF_VERSION_PATCH;
